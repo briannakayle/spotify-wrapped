01865b27e556753b81b44d68ceeb486d9eca96be
Brianna <65835059+briannakayle@users.noreply.github.com>
2022-02-01 10:14:15 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# spotify-wrapped
+
+Glide app link: https://my-spotify-wrapped.glideapp.io/
---
+++ spotifywrapped.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import pandas as pd
+import time
+import datetime
+import gspread
+
+SPOTIPY_CLIENT_ID='removed'
+SPOTIPY_CLIENT_SECRET='removed'
+SPOTIPY_REDIRECT_URI='removed '
+SCOPE = "user-top-read"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET,redirect_uri=SPOTIPY_REDIRECT_URI,scope=SCOPE))
+
+def get_track_ids(time_frame):
+    track_ids = []
+    for song in time_frame['items']:
+        track_ids.append(song['id'])
+    return track_ids
+
+def get_track_features(id):
+    meta = sp.track(id)
+    # meta
+    name = meta['name']
+    album = meta['album']['name']
+    artist = meta['album']['artists'][0]['name']
+    spotify_url = meta['external_urls']['spotify']
+    album_cover = meta['album']['images'][0]['url']
+    track_info = [name,album,artist, spotify_url, album_cover]
+    return track_info
+
+
+# test run: loop over track ids
+tracks = []
+for i in range(len(track_ids)):
+        time.sleep(.5)
+        track = get_track_features(track_ids[i])
+        tracks.append(track)                              
+
+
+def insert_to_gsheet(track_ids):
+ # loop over track ids 
+ tracks = []
+ for i in range(len(track_ids)):
+     time.sleep(.5)
+     track = get_track_features(track_ids[i])
+     tracks.append(track)
+ # create dataset
+ df = pd.DataFrame(tracks, columns = ['name', 'album', 'artist', 'spotify_url', 'album_cover'])
+ # insert into google sheet
+ gc = gspread.service_account(filename='removed')
+ sh = gc.open('My Spotify Wrapped')
+ worksheet = sh.worksheet(f'{time_period}')
+ worksheet.update([df.columns.values.tolist()] + df.values.tolist())
+
+def get_artist_ids(time_frame):
+    artist_ids = []
+    for artist in time_frame['items']:
+        artist_ids.append(artist['id'])
+    return artist_ids
+
+def get_artist_features(id):
+    meta = sp.artist(id)
+    # meta
+    name = meta['name']
+    spotify_url = meta['external_urls']['spotify']
+    artist_image = meta['images'][0]['url']
+    artist_info = [name,spotify_url, artist_image]
+    return artist_info
+
+#insert artists to gsheet
+def insert_to_gsheet(artist_ids):
+     # loop over track ids 
+     artists = []
+     for i in range(len(artist_ids)):
+         time.sleep(.5)
+         artist = get_artist_features(artist_ids[i])
+         artists.append(artist)
+     # create dataset
+     artists_df = pd.DataFrame(artists, columns = ['name','spotify_url', 'aartist_image'])
+     # insert into google sheet
+     gc = gspread.service_account(filename='removed')
+     sh = gc.open('My Spotify Wrapped')
+     worksheet = sh.worksheet('artists_' + f'{time_period}')
+     worksheet.update([artists_df.columns.values.tolist()] + artists_df.values.tolist())
+
+time_ranges = ['short_term', 'medium_term', 'long_term']
+
+for time_period in time_ranges:
+     top_artists = sp.current_user_top_artists(limit=20, offset=0, time_range=time_period)
+     artist_ids = get_artist_ids(top_artists)
+     insert_to_gsheet(artist_ids)
+
+for time_period in time_ranges:
+ top_tracks = sp.current_user_top_tracks(limit=20, offset=0, time_range=time_period)
+ track_ids = get_track_ids(top_tracks)
+ insert_to_gsheet(track_ids)
+
+
+
+
